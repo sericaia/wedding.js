49abfcba1ac9e8d5d3a437ce32b7c7f394062c14
Daniela Borges <alunassertiva@gmail.com>
2016-07-13 11:45:13 +0100

    fix simple mobile concerns

--- client/js/components/WeddingApp.jsx
+++ client/js/components/WeddingApp.jsx
@@ -9,25 +9,51 @@ export default class WeddingApp extends React.Component {
     this.state = {};
   }
 
+  isSmartPhone() {
+    var width = window.fakeInnerWidth || window.innerWidth;
+    return width <= 568;
+  }
+
+  getPictureStyle() {
+    if (this.isSmartPhone()) {
+      return {
+        width: '100%'
+      };
+    } else {
+      return {
+        width: '70%'
+      }
+    }
+  }
+
+  getContentWrapperStyle() {
+    var contentWrapper = {
+      backgroundColor: '#72a8ab',
+      display: '-ms-flex',
+      display: '-webkit-flex',
+      display: 'flex',
+      padding: '30px 30px 0px 0px'
+    };
+
+    if (this.isSmartPhone()) {
+      contentWrapper.padding = '30px 30px 0px 30px';
+      contentWrapper.flexDirection = 'column';
+    }
+
+    return contentWrapper;
+  }
+
   render () {
     return (
       <div>
-        <div style={{
-          backgroundColor: '#72a8ab',
-          display: '-ms-flex',
-          display: '-webkit-flex',
-          display: 'flex',
-          padding: '30px 30px 0px 0px'
-        }}>
-          <div style={{
-            width: '30%'
-          }}>
+        <div style={this.getContentWrapperStyle()}>
+          <div style={
+            this.isSmartPhone() ? {width: '100%'} : {width: '30%'}
+          }>
             <img src="/assets/logotipo.png" style={{width: '100%'}}/>
             <PictureForm />
           </div>
-          <div style={{
-            width: '70%'
-          }}>
+          <div style={this.getPictureStyle()}>
             <PictureList />
           </div>
         </div>
